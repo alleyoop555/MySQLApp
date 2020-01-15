bdb93902739f90c83b8164cddee2b8cc9976cce9
alleyoop555 <guowei1595@gmail.com>
2020-01-15 11:47:45 +0800

    Connecting root and goway account success

--- MySQLApp/MySQLApp/Program.cs
+++ MySQLApp/MySQLApp/Program.cs
@@ -4,12 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace MySQLApp
 {
     class Program
     {
+        static char option;
+
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("請選擇: (1) 登入  (2) 申請帳號\n");
+                Console.Write("您的選擇: ");
+                option = Console.ReadKey().KeyChar;
+
+                switch (option)
+                {
+                    case '1':
+                        string id; string password;
+                        Console.Write("\n請輸入您的帳號: ");
+                        id = Console.ReadLine();
+                        Console.Write("請輸入您的密碼: ");
+                        password = Console.ReadLine();
+                        Console.Write("\n");
+                        var signIn = new SignIn(id, password);
+                        break;
+
+                    case '2':
+                        Console.WriteLine("\n暫代");
+                        Console.Write("\n");
+                        break;
+
+                    default:
+                        Console.WriteLine("\n輸入無效，請輸入數字1或2");
+                        Console.Write("\n");
+                        break;
+                }
+            }
         }
     }
 }
---
+++ MySQLApp/MySQLApp/SignIn.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace MySQLApp
+{
+    public class SignIn
+    {
+        public string ID { get; set; }
+        public string Password { get; set; }
+
+        #region constructor
+        public SignIn(string id, string password)
+        {
+            this.ID = id;
+            this.Password = password;
+
+            string logString = "server=localhost;database=testdb;userid=" + this.ID + ";password=" + this.Password;
+            Console.WriteLine(logString);
+            MySqlConnection con = null;
+
+            try
+            {
+                con = new MySqlConnection(logString);
+                con.Open();
+                Console.WriteLine("登入成功");
+            }
+
+            finally
+            {
+                if (con != null)
+                {
+                    con.Close();
+                }
+            }
+
+        }
+        #endregion
+    }
+}
