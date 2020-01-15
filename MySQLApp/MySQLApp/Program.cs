41b3eefc793ef8cff4e723b37b99aa7481ad22b1
alleyoop555 <guowei1595@gmail.com>
2020-01-15 11:47:45 +0800

    Connecting existing account success and resuming if failed

--- MySQLApp/MySQLApp/Program.cs
+++ MySQLApp/MySQLApp/Program.cs
@@ -4,12 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
 namespace MySQLApp
 {
     class Program
     {
+        static char option;
+
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("請選擇: (1) 登入  (2) 申請帳號\n");
+                Console.Write("您的選擇: ");
+                option = Console.ReadKey().KeyChar;
+
+                switch (option)
+                {
+                    case '1':
+                        string id; string password;
+                        Console.Write("\n請輸入您的帳號: ");
+                        id = Console.ReadLine();
+                        Console.Write("請輸入您的密碼: ");
+                        password = Console.ReadLine();
+                        Console.Write("\n");
+                        var signIn = new SignIn(id, password);
+                        if (signIn.Pass)
+                        {
+                            // 進行指令輸入 (顯示資料庫中所有表單)
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("請重新輸入您的帳號及密碼或申請帳號");
+                            break;
+                        }
+
+                    case '2':
+                        Console.WriteLine("\n暫代");
+                        Console.Write("\n");
+                        break;
+
+                    default:
+                        Console.WriteLine("\n輸入無效，請輸入數字1或2");
+                        Console.Write("\n");
+                        break;
+                }
+            }
         }
     }
 }
---
+++ MySQLApp/MySQLApp/SignIn.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace MySQLApp
+{
+    public class SignIn
+    {
+        public string ID { get; set; }
+        public string Password { get; set; }
+        public bool Pass = false;
+
+        #region constructor
+        public SignIn(string id, string password)
+        {
+            this.ID = id;
+            this.Password = password;
+
+
+            string logString = "server=localhost;database=testdb;userid=" + this.ID + ";password=" + this.Password;
+            string cmdText;
+            MySqlConnection con = null;
+            MySqlDataReader reader = null;
+
+            try
+            {
+                con = new MySqlConnection(logString);
+                con.Open();
+                Console.WriteLine("登入成功");
+                Console.WriteLine("----------");
+            }
+
+            catch (MySqlException err)
+            {
+                this.Pass = false;
+                Console.WriteLine("登入失敗");
+                //Console.WriteLine(err);
+            }
+
+            finally
+            {
+                if (con != null)
+                {
+                    con.Close();
+                }
+            }
+
+        }
+        #endregion
+    }
+}
